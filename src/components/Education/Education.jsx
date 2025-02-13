99d6dd5e8711cd0c2e53b266721c6672c350e80f
Lais Gabrielle Lodi <gabriellelodi@gmail.com>
2025-02-13 10:18:41 -0700

    Ajudst max width for Education Section

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -8,28 +8,30 @@ export default function Education() {
 
   return (
     <section id="education">
-      <a href="#education" className="education-title">
-        <h1>Education</h1>
-      </a>
-      <ul className="education-list">
-        {education.map((ed, index) => {
-          return (<li key={"education-".concat(index.toString())}>
-            <h2>{ed.name}</h2>
-            <h3>{ed.type}</h3>
-            <h4>{ed.institution} {ed.year}</h4>
-          </li>
-        )})}
-      </ul>
-      <h2>Certifications</h2>
-      <ul className="certification-list">
-        {certifications.map((cert, index) => {
-          return <li key={"certification-".concat(index.toString())}>
-            <h2>{cert.name}</h2>
-            <h3>{cert.issuedBy}</h3>
-            <h4>{cert.issued}</h4>
-          </li>
-        })}
-      </ul>
+      <div className="max-width">
+        <a href="#education" className="education-title">
+          <h1>Education</h1>
+        </a>
+        <ul className="education-list">
+          {education.map((ed, index) => {
+            return (<li key={"education-".concat(index.toString())}>
+              <h2>{ed.name}</h2>
+              <h3>{ed.type}</h3>
+              <h4>{ed.institution} {ed.year}</h4>
+            </li>
+          )})}
+        </ul>
+        <h2>Certifications</h2>
+        <ul className="certification-list">
+          {certifications.map((cert, index) => {
+            return <li key={"certification-".concat(index.toString())}>
+              <h2>{cert.name}</h2>
+              <h3>{cert.issuedBy}</h3>
+              <h4>{cert.issued}</h4>
+            </li>
+          })}
+        </ul>
+      </div>
     </section>
   )
 };
