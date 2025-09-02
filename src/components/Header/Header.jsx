c25ffb8b5d6c215c71965ec130392c0b293ebca5
Lais Gabrielle Lodi <gabriellelodi@gmail.com>
2025-09-02 14:50:14 -0600

    Adjust About me Buttons styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,7 @@ import { RESUME } from "../../assets/data/resume.js";
 export default function Header() {
   const name = RESUME.name;
   const aboutMe = RESUME.about;
+  const linkedIn = RESUME.medias.LinkedIn;
   
   return (
     <header id="home">
@@ -13,7 +14,10 @@ export default function Header() {
           <h1>{name}</h1>
           <h3>Full Stack Developer</h3>
           <p>{aboutMe}</p>
-          <a href="#projects" className="button about-me">About me</a>
+          <div className="about-me-buttons">
+            <a href="#projects" className="button about-me">About me</a>
+            <a href={linkedIn} target="_blank" className="button linkedIn">LinkedIn</a>
+          </div>
         </div>
       </div>
     </header>
