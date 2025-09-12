9908f7029c13f9566484407b3e8dd0d855c78f06
Lais Gabrielle Lodi <gabriellelodi@gmail.com>
2025-09-12 12:40:25 -0600

    Adjust intlayer config file

--- intlayer.config.ts
+++ intlayer.config.ts
@@ -3,7 +3,7 @@ import { Locales, type IntlayerConfig } from "intlayer";
 
 const config: IntlayerConfig = {
   content: {
-    fileExtensions: ["src/assets/translations/.ts", "src/assets/translations/*.tsx"]
+    fileExtensions: ["src/assets/translations/*Content.ts"]
   },
   internationalization: {
     locales: [
