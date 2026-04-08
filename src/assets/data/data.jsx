7c665296c484ef01ee8e19e820c33d3a52ce1915
Lais Gabrielle Lodi <gabriellelodi@gmail.com>
2026-04-08 09:56:23 -0600

    Remove Soft skills from data.jsx

--- src/assets/data/data.jsx
+++ src/assets/data/data.jsx
@@ -1,12 +1,3 @@
-import mentorshipImg from '../images/soft-sill-mentorship.png';
-import creativityImg from '../images/soft-skill-creativity-innovation.png';
-import teamworkImg from '../images/soft-skill-teamwork.png';
-import communicationImg from '../images/soft-skills-communication.png';
-import empathyImg from '../images/empathy.svg';
-import feedbackImg from '../images/feedback.svg';
-import knowledgeShareImg from '../images/knowledge-share.svg';
-import investigativeImg from '../images/investigative.svg';
-import selfMotivatedImg from '../images/self-motivated.svg';
 import gitImg from '../images/git-logo.png';
 import gitHubImg from '../images/github-logo.png';
 import bashIgm from '../images/bash-shell-logo.png';
@@ -46,62 +37,6 @@ const SKILL_TYPE = {
 }
 
 export const SKILLS = {
-  "soft": [
-    {
-      image: mentorshipImg,
-      title: "Mentorship",
-      description:
-        "Mentoring is one of my passions. I've mentored co-ops at Tasktop and I felt in love with guiding and nurserishing younger minds.",
-    },
-    {
-      image: creativityImg,
-      title: 'Creativity',
-      description:
-        "I always try to think outside the box. I've created a innovative way to transfer knowledge and to lead discussions while bringing everyone torwards to speak their minds.",
-    },
-    {
-      image: teamworkImg,
-      title: "Teamwork",
-      description:
-        "I've facilitated weekly deployment process, updated documentation, transferred knowledge to another team to ensure smooth operations. I've lead discussions about how to best design and code GraphQL APIs using Django.",
-    },
-    {
-      image: communicationImg,
-      title: "Communication",
-      description:
-        "I've talked to more senior developers about how to communicate better with juniors and how to express their feedback more respectfully and considered.",
-    },
-    {
-      image: empathyImg,
-      title: "Empathy",
-      description:
-        "I am always kind to all, no matter their backgroung. I expect the best in people and try to know the best way to treat each person like they want to be treated, not like I think it's the best. Each of us has our own path and history. What makes sense to me may not make sense to you.",
-    },
-    {
-      image: feedbackImg,
-      title: "Constructive Feedback",
-      description:
-        "I try to find the best timing and best way of deliver feedback. I always correct people in private. I always compliment in public. Giving constructive feedback is not just saying what is wrong, but finding out we can do to get better together, as a team.",
-    },
-    {
-      image: selfMotivatedImg,
-      title: "Self-Motivation",
-      description:
-        "I love learning! I am always learning a new language, new technology, or new hobby. I'm constantly updading this portfolio with new projects where I can expand my capacities.",
-    },
-    {
-      image: investigativeImg,
-      title: "Investigative",
-      description:
-        "At Arctic Wolf, I refactored 1,600+ lines of legacy Golang integration tests. At Tasktop, I investigated and fixed false positive tests improving test confiability and effectiveness.",
-    },
-    {
-      image: knowledgeShareImg,
-      title: "Knowledge Sharing",
-      description:
-        "I organized and lead knowledge share meetings on deployment proccess, and paired up with colleagues to share knowledge about DjangoObjectType, customise fields, and related Models.",
-    },
-  ],
   "tech": [
     {
       image: gitImg,
