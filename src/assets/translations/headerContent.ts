bd68122481a629237e1d6aea660e66cfb2461e2a
Hel.isa R <17197528+hel-isa@users.noreply.github.com>
2026-05-29 19:05:46 -0400

    Fix French translations and enhance summary text
    Corrected French translations for 'About me' and improved summary text.

--- src/assets/translations/headerContent.ts
+++ src/assets/translations/headerContent.ts
@@ -18,19 +18,19 @@ export default {
     }),
     aboutMe: t({
       en: "About me",
-      "fr-CA": "Sur moi"
+      "fr-CA": "À propos de moi"
     }),
     summary: t({
       en: `Hello, I am a Full Stack Developer with 5+ years of experience building large-scale web 
-    applications. Proficient in ReactJS, Javascript, HTML, and CSS. Also knowledgeble in 
-    Java, Python, Relational databases, Docker, Jenkins, Linux environments and AWS. A 
+    applications. Proficient in ReactJS, JavaScript, HTML, and CSS. Also knowledgeable in 
+    Java, Python, relational databases, Docker, Jenkins, Linux environments and AWS. A 
     strong advocate for best practices. Collaborative and data-driven, passionate about 
     delivering high-quality, user-centred solutions in dynamic environments.`,
-      "fr-CA": `Bonjour, Je suis une Développeuse Full Stack avec plus de 5 ans d'expérience dans le développement 
-      d'applications Web à grande échelle. Je maîtrise ReactJS, Javascript, HTML pluis CSS.
-      Puis je possède également des connaissances em Java, Python, bases de données relationnelles, Docker, Jenkins, 
-      environnements Linux et AWS. Ardente défenseuse des meilleures pratiques. Je suis collaborativee 
-      et axée sur les données, passionnée par la fourniture de solutions de haute qualité, 
+      "fr-CA": `Bonjour, je suis une Développeuse Full Stack avec plus de 5 ans d'expérience dans le développement 
+      d'applications Web à grande échelle. Je maîtrise ReactJS, JavaScript, HTML et CSS.
+      En plus, je possède des connaissances en Java, Python, bases de données relationnelles, Docker, Jenkins, 
+      environnements Linux et AWS. Ardente défenseuse des meilleures pratiques. Je suis collaborative 
+      et axée sur les données, passionnée par la création de solutions de haute qualité, 
       centrées sur l'utilisateur, dans des environnements dynamiques.`
     }),
     linkedIn: "https://www.linkedin.com/in/laislodi/"
